da46dbcc69e164519c50694f61725f6af8405895
Omette <ometteomega@gmail.com>
2025-12-10 20:13:57 -0600

    feat: Basic grid generation function.

---
+++ script.js
@@ -0,0 +1,16 @@
+function generateGrid(size) {
+    const container = document.querySelector('.gridContainer');
+    for (let counter = 0; counter < size; counter++) {
+        const row = document.createElement('div');
+        for (let i = 0; i < size; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+    
+}
+
+
+generateGrid(16);
