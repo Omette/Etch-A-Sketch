b7f60c2489f5817893b554a366fb787d23c913c9
Omette <ometteomega@gmail.com>
2025-12-10 21:15:16 -0600

    feat: Added grid size slider.

--- script.js
+++ script.js
@@ -5,13 +5,33 @@ function generateGrid(size) {
         for (let i = 0; i < size; i++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.draggable = false;
+            cell.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = 'black';
+            });
+            row.draggable = false;
             row.appendChild(cell);
         }
         row.classList.add('row');
         container.appendChild(row);
     }
-    
+}
+
+function clearGrid() {
+    const container = document.querySelector('.gridContainer');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function updateGrid(slider, newSize) {
+    slider.nextElementSibling.value = newSize;
+    clearGrid();
+    generateGrid(newSize);
 }
 
 
-generateGrid(16);
+
+//const size = getSizeInput();
+generateGrid(20);
+
